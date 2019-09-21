2a41d83ada757c4b9d9f1b5a9a85a62a0caa6caa
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-09-21 17:27:09 +0200

    feat: best article input

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // service worker registration - remove if you're not going to use it
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
@@ -15,8 +14,6 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
-
-
 const about__header = document.querySelector('.about__header');
 console.log(about__header);
 console.log(about__header.innerHTML);
@@ -26,22 +23,7 @@ about__header.innerHTML = 'Kilka słów o mnie JS';
 const about__paragraph = document.querySelector('.about__paragraph--js');
 about__paragraph.innerHTML = "Pozdro z JS!";
 
-
-
-function greet(name, age) {
-  console.log(`Witaj ${name}, masz ${age} lat:) Dzięki, że jesteś`);
-}
-greet('Ula', 15);
-
-const welcome = (name, age) => {
-  console.log(`Witaj ${name}, masz ${age} lat:)`);
-}
-welcome('ula', 15);
-
-const name = 'ula';
-const age = 40;
-welcome(name, age);
-
+// button zmień treść
 const button = document.querySelector(".header__button--js");
 const header = document.querySelector(".header__text--js");
 
@@ -51,12 +33,12 @@ function changeHeader() {
 }
 button.addEventListener('click', changeHeader);
 
+//menu hamburger
 const hamburgerMenu = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation__list');
 const iconBurger = document.querySelector('.fa-bars');
 const iconX = document.querySelector('.fa-times');
 // const header = document.querySelector('.header') - MAM JUŻ TĄ ZMIENNĄ!;
-
 hamburgerMenu.addEventListener('click', function () {
   iconBurger.classList.toggle('offIcon');
   iconX.classList.toggle('offIcon');
@@ -65,16 +47,4 @@ hamburgerMenu.addEventListener('click', function () {
   header.classList.toggle('marginGoesDown');
 });
 
-
-const person = {
-  name: 'ula',
-  age: 40,
-  city: 'wroclaw'
-}
-
-console.log(person);
-
-const jsonPerson = JSON.stringify(person);
-console.log(jsonPerson);
-
-localStorage.setItem('person', jsonPerson);
+//input z pozostającym tekstem
